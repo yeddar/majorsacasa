5cc1eaa34712baf3171e1706c2a9097dfded2ccf
Alvaro Guevara <al361914@uji.es>
2020-03-06 17:19:16 +0100

    Created RespEmpresa files (Model, RowMapper, Dao).

---
+++ src/main/java/es/uji/ei1027/majorsacasa/dao/RespEmpresaDao.java
@@ -0,0 +1,60 @@
+package es.uji.ei1027.majorsacasa.dao;
+
+import es.uji.ei1027.majorsacasa.model.RespEmpresa;
+import org.springframework.beans.factory.annotation. Autowired ;
+import org.springframework.dao.DataAccessException;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype. Repository ;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RespEmpresaDao {
+    private JdbcTemplate jdbcTemplate ;
+    private static String TABLA = "resp_empresa";
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    void addRespEmpresa(RespEmpresa d) throws DataAccessException {
+        jdbcTemplate.update(
+                "INSERT INTO "+TABLA+" VALUES(?, ?, ?, ?, ?)",
+                d.getNick(), d.getNombre(), d.getCif(), d.getTlf(), d.getCorreo()
+        );
+    }
+
+    void deleteRespEmpresa (RespEmpresa d) {
+        jdbcTemplate.update("DELETE FROM "+TABLA+" WHERE nick=?", d.getNick());
+    }
+
+    public RespEmpresa getRespEmpresa(String nickRespEmpresa) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM "+TABLA+" WHERE nick=?",
+                    new RespEmpresaRowMapper(),
+                    nickRespEmpresa);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return null ;
+        }
+    }
+
+    void updateRespEmpresa(RespEmpresa n) {
+        jdbcTemplate .update( "UPDATE "+TABLA+" SET nombre=?, cif=?, tlf=?, correo=?" +
+                " WHERE nick=?", n.getNombre(), n.getCif(), n.getTlf(), n.getCorreo(), n.getNick());
+    }
+
+    public List<RespEmpresa> getRespsEmpresas() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM "+TABLA,
+                    new RespEmpresaRowMapper());
+        }
+        catch (Exception e) {
+            return new ArrayList<RespEmpresa>();
+        }
+    }
+}
---
+++ src/main/java/es/uji/ei1027/majorsacasa/model/RespEmpresa.java
@@ -0,0 +1,64 @@
+package es.uji.ei1027.majorsacasa.model;
+
+public class RespEmpresa {
+    String nick;
+    String nombre;
+    String cif;
+    String tlf;
+    String correo;
+
+    public RespEmpresa(){
+        super();
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "RespEmpresa{" +
+                "nick='" + nick + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cif='" + cif + '\'' +
+                ", tlf='" + tlf + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
