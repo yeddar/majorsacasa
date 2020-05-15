3e88e1a055569f74610ca98e25000f1d182efe1c
mocker <al258002@uji.es>
2020-05-15 20:23:41 +0200

    Loggeo con Diseño

---
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/CommonController.java
@@ -0,0 +1,26 @@
+package es.uji.ei1027.majorsacasa.controller;
+
+import es.uji.ei1027.majorsacasa.dao.UsuarioDao;
+import es.uji.ei1027.majorsacasa.model.Demandante;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/common")
+public class CommonController {
+
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    public void setUsuarioDao(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    @RequestMapping(value = "/rootIndex")
+    public String allView(Model model) {
+        return "root/rootIndex";
+    }
+
+}
