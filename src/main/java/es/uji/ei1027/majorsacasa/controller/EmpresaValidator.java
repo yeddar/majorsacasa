3f9c37fa35f78a58cad915b139fc40e07a00312c
Alvaro Guevara <al361914@uji.es>
2020-06-03 20:59:11 +0200

    HTML tags updated for -> empresa\add.html Also updated validator to check NIF correct values.

--- src/main/java/es/uji/ei1027/majorsacasa/controller/EmpresaValidator.java
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/EmpresaValidator.java
@@ -37,8 +37,35 @@ public class EmpresaValidator implements Validator {
 
         // nick
         if(!emp.getNick().matches("^[A-Za-z][A-Za-z0-9]*$"))
-            errors.rejectValue("nick", "inválido", "El nick no puede contener espacios en blanco");
+            errors.rejectValue("nick", "inválido", "El nick no puede contener espacios en blanco.");
+
+        // nif
+        if(!isNifNumValid(emp.getNif()))
+            errors.rejectValue("nif", "inválido", "El NIF no es válido.");
 
 
     }
+
+    public static boolean isNifNumValid(String nif){
+        //Si el largo del NIF es diferente a 9, acaba el método.
+        if (nif.length()!=9){
+            return false;
+        }
+
+        String secuenciaLetrasNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+        nif = nif.toUpperCase();
+
+        //Posición inicial: 0 (primero en la cadena de texto).
+        //Longitud: cadena de texto menos última posición. Así obtenemos solo el número.
+        String numeroNIF = nif.substring(0, nif.length()-1);
+
+        //Si es un NIE reemplazamos letra inicial por su valor numérico.
+        numeroNIF = numeroNIF.replace("X", "0").replace("Y", "1").replace("Z", "2");
+
+        //Obtenemos la letra con un char que nos servirá también para el índice de las secuenciaLetrasNIF
+        char letraNIF = nif.charAt(8);
+        int i = Integer.parseInt(numeroNIF) % 23;
+        return letraNIF == secuenciaLetrasNIF.charAt(i);
+    }
+
 }
